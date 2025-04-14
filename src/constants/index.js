b9c0a8e60dcf749099471b913e311b3c8383a5ab
jasFactacy <jas@factacy.ai>
2025-04-14 15:43:55 +0530

    ...

--- src/constants/index.js
+++ src/constants/index.js
@@ -38,10 +38,10 @@ export const navLinks = [
     id: "work",
     title: "Work",
   },
-  {
-    id: "projects",
-    title: "Projects",
-  },
+  // {
+  //   id: "projects",
+  //   title: "Projects",
+  // },
   {
     id: "contact",
     title: "Contact",
