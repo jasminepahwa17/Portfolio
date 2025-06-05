d3ec51f16fcfece1d9c3944a796e32021632090d
Jasmine Pahwa <jasminepahwa4567@gmail.com>
2025-06-05 14:46:05 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
-
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App));
